708c59fb8ebb120007b625be4fee01e58318711a
Rodrigo Diaz <rodrigo.diaz@yahoo.com>
2018-02-25 15:23:46 -0600

    My first iOS app

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -10,6 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myLabel: UILabel!
+    
+    var tapCount: Int = 0;
+    
+    @IBAction func buttonTapped(_ sender: Any) {
+        tapCount += 1;
+        self.myLabel.text = String(tapCount)
+        print(tapCount)
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
